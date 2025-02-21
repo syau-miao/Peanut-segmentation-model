4c55ca799e3a0a424820978220d047b4273a56fa
zhangbo <1399751778@qq.com>
2025-02-21 21:48:09 +0800

    Changes

--- src/PH.py
+++ src/PH.py
@@ -6,15 +6,30 @@ import math
 import open3d as o3d
 import openpyxl
 
-def P_HW(data):
+def mean_absolute_percentage_error(y_true, y_pred):
+    '''
+
+    :param y_true: True value
+    :param y_pred: Predicted value
+    :return: MAPE
+    '''
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    # Avoid division by zero
+    y_true = np.where(y_true == 0, np.finfo(float).eps, y_true)  # Replace 0 with a very small number
+    mape = np.mean(np.abs((y_true - y_pred) / y_true))
+    return mape
+
+def P_HW(data,points,num):
     '''
 
     :param data: Plant point cloud data
-    :return:Plant height and width
+    :param points: Pot point cloud data
+    :param num: The column where the label is located
+    :return:Plant height
     '''
     xyzl=data
     print(xyzl)
-    points = xyzl[xyzl[:, -2] == 1]
+    points = points
     points = points[:, :3]
     max_z = np.max(points[:, 2])
     min_z = np.min(points[:, 2])
@@ -36,22 +51,13 @@ def P_HW(data):
     pcd_terr = o3d.geometry.KDTreeFlann(project_cloud)
     [k1, idx1, _] = pcd_terr.search_radius_vector_3d(project_cloud.points[-1], distance - 5)
     points = xyzl[idx1[1:]]
-    point1 = points[points[:, -2] == 1]
-    point_o = points[np.argmin(points[:, 2])]
-    xyz = xyzl[:, :3]
-    xyzl = xyzl[xyzl[:, -2] != 1]
+    point1 = points[points[:, num] !=0]
+    point_o = point1[np.argmin(point1[:, 2])]
+    xyzl = xyzl[xyzl[:, num] != 0]
     point_l = np.argmax(xyzl[:, 2])
-    point1 = np.argmax(xyzl[:, 1])
-    point2 = np.argmax(xyzl[:, 0])
-    point11 = np.argmin(xyzl[:, 1])
-    point22 = np.argmin(xyzl[:, 0])
     h = xyzl[point_l][2] - point_o[2]
-    w1 = math.sqrt((xyzl[point1][1] - xyzl[point11][1]) ** 2 + (xyzl[point1][0] - xyzl[point11][0]) ** 2)
-    w2 = math.sqrt((xyzl[point2][0] - xyzl[point22][0]) ** 2 + (xyzl[point2][1] - xyzl[point22][1]) ** 2)
-    if w1 > w2:
-        return h,w1
-    else:
-        return h,w2
+    return h
+
 
 def max_euclidean_distance(base_point, points):
     '''
@@ -60,6 +66,7 @@ def max_euclidean_distance(base_point, points):
     :param points:PointS in a point cloud
     :return:Mean distance
     '''
+    # 将列表转换为NumPy数组以进行向量化操作
     base_point = np.array(base_point)
     other_points = np.array(points)
 
@@ -79,31 +86,46 @@ def calculate_centroid(points):
     - points: An array of point clouds with the shape (N, 3), where N is the number of points.
     - centroid: Centroid coordinates.
     """
+    # 计算质心
     centroid = np.mean(points, axis=0)
     return centroid
 
 def main():
-    name=['ID']
-    leng=['株高']
-    width=['真实株幅']
-    data_root="F:\/240823_n40_data_train_val_test_result\普通手动真值"
+    name = ['ID']
+    leng = ['株高']
+    leng_pred = ['pred_high']
+    data_root = "D:\新花生\stem\/result\sem_pred"
     for item in os.listdir(data_root):
+        print(item)
         name.append(item)
-        xyzl=np.loadtxt(os.path.join(data_root,item))
-        h,w=P_HW(xyzl)
-        print(h,w)
-        width.append(w)
+        xyzl = np.loadtxt(os.path.join(data_root, item))
+        points = xyzl[xyzl[:, -1] == 0]
+        h = P_HW(xyzl, points, -1)
+
         leng.append(h)
+        points_pred = xyzl[xyzl[:, -2] == 0]
+        h_pred = P_HW(xyzl, points_pred, -2)
+        # print(h, w)
+        leng_pred.append(h_pred)
+
+
+
     wb = openpyxl.Workbook()
     sheet = wb.active
-    data=[name,leng,width]
-    id=1
+    name.append("")
+    mape_h = mean_absolute_percentage_error(leng[1:], leng_pred[1:])
+
+    leng.append("MAPE")
+    leng_pred.append(mape_h)
+
+    data = [name, leng, leng_pred]
+    id = 1
     for item in range(len(data)):
         index = id + item
         for i in range(len(data[item])):
             sheet.cell(row=i + 1, column=index, value=data[item][i])
     print(name)
-    wb.save('株高株幅预测.xlsx')
+    wb.save('D:\py\表型\RESULT\株高预测.xlsx')
 
 if __name__=='__main__':
     main()
